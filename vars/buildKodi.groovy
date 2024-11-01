58d9214883e11e0235f4ff5c11f1ad7caf7bac04
wsnipex <wsnipex@a1.net>
2024-11-01 06:32:26 +0100

    Reapply "buildKodi: add ndk 27"
    This reverts commit a9477d3ec4e32b2d751b1b58f671daba9805e890.

--- vars/buildKodi.groovy
+++ vars/buildKodi.groovy
@@ -22,7 +22,7 @@ def call(Map buildParams = [:]) {
     def toolChain = buildParams.containsKey('toolchain_path') ? buildParams.toolchain_path : '/usr'
 
     // Android
-    def ndksValid = ['26.2.11394342', '21.4.7075529']
+    def ndksValid = ['27.2.12479018', '26.2.11394342', '21.4.7075529']
     def ndkChoices = platform =~ /Android/ ? ndksValid : 'ignored'
     def ndkVer = env.NDK_VERSION ?: (buildParams.containsKey('ndk_version') ? buildParams.ndk_version : params.NDK)
     def sdkPath = buildParams.containsKey('sdk_path') ? buildParams.sdk_path : '/home/jenkins/android-tools/android-sdk-linux'
